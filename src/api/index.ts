654de26398754453c68a53f1d0c15ed19a010b34
Alexandre Bertuol <axdber@gmail.com>
2022-05-25 18:57:52 -0300

    remove console.logs

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,5 +5,4 @@ export const getCepInfo = ({ cep }: { cep: string }) => {
   console.log("c", cep);
   return axios.get(`http://viacep.com.br/ws/${cep}/json`)
     .then((r: AxiosResponse<GetCepInfoResponse>) => r.data)
-  // .catch((e) => console.error(e));
 }
--- src/services/cep-service.ts
+++ src/services/cep-service.ts
@@ -17,21 +17,16 @@ interface CepServiceInterface {
 export class CepService implements CepServiceInterface {
   constructor() { }
   async getSyncedCepsInfo(ceps: CepsBody) {
-    console.log(ceps)
-    Object.entries(ceps).forEach((([key, value]) => console.log("xx", key, "xxx", value)))
     let promises = Object.entries(ceps)
       .filter(([key, value]) => value.length === 8)
       .map(([key, value]) => {
-        console.log(key, value)
         return async () => getCepInfo({ cep: value })
       })
-    console.log("oi", promises)
     let result = Promise.resolve()
     const viaCepsResponses: GetCepInfoResponse[] = []
     promises.forEach(function (promise) {
       result = result.then(promise)
         .then((res) => {
-          console.log("x", res)
           viaCepsResponses.push(res)
           return Promise.resolve()
         })
